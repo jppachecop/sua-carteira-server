92e48e3e158a7c9fd558b55d03ff744ada07522b
Joao Paulo Pacheco <jppacheco09@hotmail.com>
2023-07-26 23:54:59 -0300

    feat: create contributions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContributionsController } from './contributions/contributions.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ContributionsController],
   providers: [AppService],
 })
 export class AppModule {}
